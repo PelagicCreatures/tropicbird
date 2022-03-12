914684d2fe43a8943c80ce8c3c357bc927c7c908
Michael Rhodes <mrhodes@mediapolis.com>
2022-03-12 13:54:45 -0400

    sorta works

--- components/button.js
+++ components/button.js
@@ -1,43 +1,44 @@
 import { Sargasso, utils } from '@pelagiccreatures/sargasso'
 import { html, render } from 'lit-html'
-import { MDCRipple } from 'material-components-web/node_modules/@material/ripple'
+import { MDCRipple } from '@material/ripple'
 
-class ButtonComponent extends Sargasso {
+class MDCComponent extends Sargasso {
 	constructor (element, options) {
 		options.shadowDOM = true
 		super(element, options)
 
-		this.pendingLinkTagCount = 2 // pending css files in component
+		this.toDestroy = [] // push any MDC things you instantiate into this array so they can be destroyed on sleep
 
 		// attributes that affect structure of template - requires template rebuild and render
-		this.templateAttributes = ['raised', 'unelevated', 'outlined', 'icon-leading', 'icon-trailing', 'icon']
+		this.templateAttributes = []
 
 		// observed attributes - change will trigger render only
-		this.renderAttributes = ['label','disabled']
-
-		// all attributes to observe
-		this.watchAttributes = this.templateAttributes.concat(this.renderAttributes)
+		this.renderAttributes = []
 
 		// current attribute values
 		this.templateOptions = {}
 		this.renderOptions = this.observableStart('MDCOptions-' + this.uid, {})
-
-		this.getAttributes() // get attributes from host element
 	}
 
 	start () {
 		super.start()
-
-		this.elegant() // set up graceful rendering kludge
+		this.getAttributes()
+		this.watchAttributes() // watch attributes on host element
 		this.setTemplateArgs(this.renderOptions.data)
 		this.buildTemplate() // build and install lit-html template
+		this.elegant() // set graceful rendering kludge
 		this.setRenderer(render) // set lit-html render() as the renderer
+	}
+
+	watchAttributes () {
+		// all attributes to observe
+		this.allAttributes = this.templateAttributes.concat(this.renderAttributes)
 
 		// watch for changes to host element attributes
 		this.attributeObserver = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
 				if (mutation.type === "attributes") {
-					if(this.watchAttributes.indexOf(mutation.attributeName) !== -1) {
+					if(this.allAttributes.indexOf(mutation.attributeName) !== -1) {
 						this.getAttributes()
 						if(this.templateAttributes.indexOf(mutation.attributeName) !== -1) {
 							this.buildTemplate()
@@ -64,29 +65,6 @@ class ButtonComponent extends Sargasso {
 	}
 
 	buildTemplate () {
-		let iconLeading = html``
-		let iconTrailing  = html``
-		var classes = ['mdc-button']
-		if(this.templateOptions.raised) {
-			classes.push('mdc-button--raised')
-		}
-		if(this.templateOptions.unelevated) {
-			classes.push('mdc-button--unelevated')
-		}
-		if(this.templateOptions.outlined) {
-			classes.push('mdc-button--outlined')
-		}
-		if(this.templateOptions['icon-leading'] && this.templateOptions.icon) {
-			classes.push('mdc-button--icon-leading')
-			iconLeading = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
-		}
-		if(this.templateOptions['icon-trailing'] && this.templateOptions.icon) {
-			classes.push('mdc-button--icon-trailing')
-			iconTrailing = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
-		}
-
-		const buttonClasses = classes.join(' ')
-
 		const template = args => html`
 			<style>
 				.web-component-body {
@@ -95,50 +73,44 @@ class ButtonComponent extends Sargasso {
 				}
 				.web-component-body--loaded { opacity: 1; }
 			</style>
-			<link href="/dist/button.css" rel="stylesheet">
-			<link href="/dist/icons.css" rel="stylesheet">
 			<div class="web-component-body">
-				<div class="mdc-touch-target-wrapper">
-					<button class="${buttonClasses}" ?disabled=${args.disabled}>
-						<span class="mdc-button__ripple"></span>
-						<span class="mdc-button__touch"></span>
-						${iconLeading}
-						<span class="mdc-button__label">${args.label}</span>
-						${iconTrailing}
-					</button>
-				</div>
+				<p>default buildTemplate - override buildTemplate</p>
 			</div>
 		`
 		this.setTemplate(template) // set template function
 	}
 
 	sleep () {
-		if(this.buttonRipple) { this.buttonRipple.destroy() }
+		if(this.toDestroy) {
+			for(const destroyable of this.toDestroy) {
+				destroyable.destroy()
+			}
+		}
 
 		if(this.attributeObserver) {
 			this.attributeObserver.disconnect();
 		}
+
 		if(this.styleObserver) {
 			this.styleObserver.disconnect();
 		}
 		super.sleep()
 	}
 
+	// do any needed mdc instantiations here - don't forget to destroy them in sleep()
+
+	instantiateMDC () {}
+
 	_render () {
 		super._render()
-
-		// do any needed mdc instantiations here
-
-		let el = this.element.querySelector('.mdc-button')
-		if(el && !el.classList.contains('mdc-ripple-upgraded ')) {
-			if(this.buttonRipple) { this.buttonRipple.destroy() }
-			this.buttonRipple = new MDCRipple(el);
-		}
+		this.pendingLinkTagCount = this.element.querySelectorAll('link').length
+		this.instantiateMDC()
 	}
 
 	elegant () {
 
-		// convoluted scheme to cover janky rendering by hiding element until all the css is ready within the component
+		// convoluted scheme to cover janky rendering
+		// hide element until all the css is ready within the component
 
 		var nodes = ["LINK"];
 
@@ -167,8 +139,83 @@ class ButtonComponent extends Sargasso {
 	}
 }
 
+utils.registerSargassoClass('MDCComponent', MDCComponent)
+
+class ButtonComponent extends MDCComponent {
+	constructor (element, options) {
+		options.shadowDOM = true
+		super(element, options)
+
+		// attributes that affect structure of template - requires template rebuild and render
+		this.templateAttributes = ['raised', 'unelevated', 'outlined', 'icon-leading', 'icon-trailing', 'icon']
+
+		// observed attributes - change will trigger render only
+		this.renderAttributes = ['label','disabled']
+	}
+
+	buildTemplate () {
+
+		let iconLeading = html``
+		let iconTrailing  = html``
+		var classes = ['mdc-button']
+		if(this.templateOptions.raised) {
+			classes.push('mdc-button--raised')
+		}
+		if(this.templateOptions.unelevated) {
+			classes.push('mdc-button--unelevated')
+		}
+		if(this.templateOptions.outlined) {
+			classes.push('mdc-button--outlined')
+		}
+		if(this.templateOptions['icon-leading'] && this.templateOptions.icon) {
+			classes.push('mdc-button--icon-leading')
+			iconLeading = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
+		}
+		if(this.templateOptions['icon-trailing'] && this.templateOptions.icon) {
+			classes.push('mdc-button--icon-trailing')
+			iconTrailing = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
+		}
+
+		const buttonClasses = classes.join(' ')
+
+		const template = (args) => html`
+			<style>
+				.web-component-body {
+					opacity: 0;
+					transition: opacity .25s ease-in-out;
+				}
+				.web-component-body--loaded { opacity: 1; }
+			</style>
+			<link href="/dist/button.css" rel="stylesheet">
+			<link href="/dist/icons.css" rel="stylesheet">
+			<div class="web-component-body">
+				<div class="mdc-touch-target-wrapper">
+					<button class="${buttonClasses}" ?disabled=${args.disabled}>
+						<span class="mdc-button__ripple"></span>
+						<span class="mdc-button__touch"></span>
+						${iconLeading}
+						<span class="mdc-button__label">${args.label}</span>
+						${iconTrailing}
+					</button>
+				</div>
+			</div>
+		`
+		this.setTemplate(template) // set template function
+	}
+
+	instantiateMDC () {
+		let el = this.element.querySelector('.mdc-button')
+		if(el && !el.classList.contains('mdc-ripple-upgraded ')) {
+			if(this.buttonRipple) { this.buttonRipple.destroy() }
+			this.buttonRipple = new MDCRipple(el)
+			this.toDestroy.push(this.buttonRipple)
+		}
+	}
+}
+
 utils.registerSargassoClass('ButtonComponent', ButtonComponent)
 
 export {
+	MDCComponent,
 	ButtonComponent
 }
