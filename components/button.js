32de798e8f16c9047cf1c2de8d020fbda8b154f3
Michael Rhodes <mrhodes@mediapolis.com>
2022-03-12 00:44:27 -0400

    es6 packaging using parcel

--- components/button.js
+++ components/button.js
@@ -1,12 +1,13 @@
 import { Sargasso, utils } from '@pelagiccreatures/sargasso'
-
 import { html, render } from 'lit-html'
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
+import { MDCRipple } from 'material-components-web/node_modules/@material/ripple'
 
 class ButtonComponent extends Sargasso {
 	constructor (element, options) {
 		options.shadowDOM = true
 		super(element, options)
+
+		this.pendingLinkTagCount = 2 // pending css files in component
 
 		// attributes that affect structure of template - requires template rebuild and render
 		this.templateAttributes = ['raised', 'unelevated', 'outlined', 'icon-leading', 'icon-trailing', 'icon']
@@ -21,17 +22,18 @@ class ButtonComponent extends Sargasso {
 		this.templateOptions = {}
 		this.renderOptions = this.observableStart('MDCOptions-' + this.uid, {})
 
-		this.getAttributes()
+		this.getAttributes() // get attributes from host element
 	}
 
 	start () {
 		super.start()
-		
-		this.setTemplateArgs(this.getObservable("MDCOptions-" + this.uid))
-		this.buildTemplate()
+
+		this.elegant() // set up graceful rendering kludge
+		this.setTemplateArgs(this.renderOptions.data)
+		this.buildTemplate() // build and install lit-html template
 		this.setRenderer(render) // set lit-html render() as the renderer
 
-		// handle changes to host element attributes
+		// watch for changes to host element attributes
 		this.attributeObserver = new MutationObserver((mutations) => {
 			mutations.forEach((mutation) => {
 				if (mutation.type === "attributes") {
@@ -62,8 +64,8 @@ class ButtonComponent extends Sargasso {
 	}
 
 	buildTemplate () {
-		let iconLeading, iconTrailing
-
+		let iconLeading = html``
+		let iconTrailing  = html``
 		var classes = ['mdc-button']
 		if(this.templateOptions.raised) {
 			classes.push('mdc-button--raised')
@@ -74,11 +76,11 @@ class ButtonComponent extends Sargasso {
 		if(this.templateOptions.outlined) {
 			classes.push('mdc-button--outlined')
 		}
-		if(this.templateOptions['icon-leading']) {
+		if(this.templateOptions['icon-leading'] && this.templateOptions.icon) {
 			classes.push('mdc-button--icon-leading')
 			iconLeading = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
 		}
-		if(this.templateOptions['icon-trailing']) {
+		if(this.templateOptions['icon-trailing'] && this.templateOptions.icon) {
 			classes.push('mdc-button--icon-trailing')
 			iconTrailing = html`<i class="material-icons mdc-button__icon" aria-hidden="true">${this.templateOptions.icon}</i>`
 		}
@@ -86,25 +88,82 @@ class ButtonComponent extends Sargasso {
 		const buttonClasses = classes.join(' ')
 
 		const template = args => html`
+			<style>
+				.web-component-body {
+					opacity: 0;
+					transition: opacity .25s ease-in-out;
+				}
+				.web-component-body--loaded { opacity: 1; }
+			</style>
 			<link href="/dist/button.css" rel="stylesheet">
 			<link href="/dist/icons.css" rel="stylesheet">
-			<div class="mdc-touch-target-wrapper">
-				<button class="${buttonClasses}" ?disabled=${args.disabled}>
-					<span class="mdc-button__ripple"></span>
-					<span class="mdc-button__touch"></span>
-					${iconLeading}
-					<span class="mdc-button__label">${args.label}</span>
-					${iconTrailing}
-				</button>
+			<div class="web-component-body">
+				<div class="mdc-touch-target-wrapper">
+					<button class="${buttonClasses}" ?disabled=${args.disabled}>
+						<span class="mdc-button__ripple"></span>
+						<span class="mdc-button__touch"></span>
+						${iconLeading}
+						<span class="mdc-button__label">${args.label}</span>
+						${iconTrailing}
+					</button>
+				</div>
 			</div>
 		`
-
 		this.setTemplate(template) // set template function
 	}
 
 	sleep () {
-		this.attributeObserver.disconnect();
+		if(this.buttonRipple) { this.buttonRipple.destroy() }
+
+		if(this.attributeObserver) {
+			this.attributeObserver.disconnect();
+		}
+		if(this.styleObserver) {
+			this.styleObserver.disconnect();
+		}
 		super.sleep()
+	}
+
+	_render () {
+		super._render()
+
+		// do any needed mdc instantiations here
+
+		let el = this.element.querySelector('.mdc-button')
+		if(el && !el.classList.contains('mdc-ripple-upgraded ')) {
+			if(this.buttonRipple) { this.buttonRipple.destroy() }
+			this.buttonRipple = new MDCRipple(el);
+		}
+	}
+
+	elegant () {
+
+		// convoluted scheme to cover janky rendering by hiding element until all the css is ready within the component
+
+		var nodes = ["LINK"];
+
+		var nodeLoaded = (e) => {
+			e.target.removeEventListener("load", nodeLoaded);
+			if(!--this.pendingLinkTagCount) {
+				this.element.querySelector('.web-component-body').classList.add('web-component-body--loaded')
+				this.styleObserver.disconnect()
+			}
+		}
+
+		this.styleObserver = new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				for (var i = 0; i < mutation.addedNodes.length; i++) {
+					var node = mutation.addedNodes[i];
+					if (nodes.indexOf(node.nodeName.toUpperCase()) !== -1) {
+						node.addEventListener("load", nodeLoaded);
+					}
+				}
+			})
+		})
+
+		this.styleObserver.observe(this.element, {
+			childList:true
+		});
 	}
 }
 
